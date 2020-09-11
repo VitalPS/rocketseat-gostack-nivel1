ccfee868ff0d8462f60709bdf5faf65b1a7c8503
Patrik da Silva Vital <patrikdasilvavital@gmail.com>
2020-09-10 22:35:58 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json("Hello World!");
+app.get("/projects/", (request, response) => {
+  return response.json(["project 1", "project 2"]);
+});
+
+app.post("/projects", (request, response) => {
+  return response.json(["projects 1", "projects 2", "projects 3"]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["project 4", "project 2", "project 3"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["projects 2", "projects 3"]);
 });
 
 app.listen(3333, () => {
